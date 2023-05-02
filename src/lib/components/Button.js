e842e84bb8122079b9e2a7706c9b03876719c5dc
rowinbot <rowinkaleb@gmail.com>
2023-05-02 03:24:18 +0100

    Support anchors in Button comp + more props

--- src/lib/components/Button.js
+++ src/lib/components/Button.js
@@ -1,6 +1,13 @@
 import PropTypes from 'prop-types';
 
-export function Button({ children, variant = 'primary' }) {
+export function Button({
+  children,
+  variant = 'primary',
+  href,
+  target,
+  rel,
+  onClick,
+}) {
   let backgroundColor;
   let color;
 
@@ -14,25 +21,34 @@ export function Button({ children, variant = 'primary' }) {
     color = '#fff';
   }
 
-  return (
-    <button
-      style={{
-        backgroundColor,
-        color,
-        border: '1px solid #000',
-        padding: '0.25rem',
-        borderRadius: '0.5rem',
-        fontSize: '1rem',
-        textTransform: 'uppercase',
-        cursor: 'pointer',
-      }}
-    >
-      {children}
-    </button>
-  );
+  const style = {
+    backgroundColor,
+    color,
+    border: '1px solid #000',
+    padding: '0.25rem',
+    borderRadius: '0.5rem',
+    fontSize: '1rem',
+    textTransform: 'upperca se',
+    cursor: 'pointer',
+  };
+
+  const isAnchor = !!href;
+  const props = isAnchor
+    ? { style, onClick, href, target, rel }
+    : {
+        style,
+        onClick,
+      };
+  const ButtonElement = isAnchor ? 'a' : 'button';
+
+  return <ButtonElement {...props}>{children}</ButtonElement>;
 }
 
 Button.propTypes = {
   children: PropTypes.elementType.isRequired,
   variant: PropTypes.oneOf(['primary', 'secondary']),
+  href: PropTypes.string,
+  target: PropTypes.string,
+  rel: PropTypes.string,
+  onClick: PropTypes.func,
 };
